a6121a744717310dd87fb62ad40729e44f1e4398
Shreyas Manoti <manotishreyas@gmail.com>
2023-07-19 13:42:24 +0530

    First Commit

--- controllers/products.js
+++ controllers/products.js
@@ -8,17 +8,88 @@ const {sendTokenResponse} = require("../controllers/auth");
 // @access      Public
 
 exports.getProducts = async (req, res, next) => {
-    req.body.user = req.user.id
-    
+  req.body.user = req.user.id
+
+  let query;
+
+  // Copy req.query
+  const reqQuery = {...req.query}
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  //Loop over removeFields and delete them from req.query
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery)
+
+  // Create operators like GT and GTE etc
+  queryStr = queryStr.replace(/\b(gt | gte|lt|lte|in)\b/g, match => `$${match}`)
+ 
+
+  // Finding Resource 
+  query = Product.find(JSON.parse(queryStr))
+
+   
+  // SELECT FIELDS
+  if(req.query.select){
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if(req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  }else{
+    query = query.sort('-createdAt');
+
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page-1) * limit;
+  const endIndex = page * limit;
+  const total = await Product.countDocuments();
+
+  
+  query = query.skip(startIndex).limit(limit);
+
+    // Executing query
+    const products = await query
+
+    // Pagination result
+    const pagination = {};
+
+    if(endIndex < total) {
+      pagination.next = {
+        page: page+1,
+        limit
+      }
+    }
+
+    if(startIndex > 0) {
+      pagination.prev = {
+        page: page-1,
+        limit
+      }
+    }
+
     const product = await Product.aggregate([
       { $match: {} }, // Match all documents
-      { $project: { _id: 0, name: 1, description: 1, price: 1, category: 1 } } // Include only specific fields
+      { $project: { _id: 0, name: 1, description: 1, price: 1 } } // Include only specific fields
     ]);
+
   
     res.status(200).json({
       success: true,
-      count: product.length,
-      data: product,
+      count: products.length,
+      pagination,
+      data: products,
     });
   };
 
